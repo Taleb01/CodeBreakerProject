7cb892870ea71201120f6de84b3f3cd17865ed04
Taleb01 <taleb_n1@hotmail.com>
2017-11-21 15:33:50 +0100

    finish the programm

--- src/assets/main.js
+++ src/assets/main.js
@@ -2,15 +2,104 @@ let answer = document.getElementById('answer');
 let attempt = document.getElementById('attempt');
 
 function guess() {
-    let input = document.getElementById('user-guess');
-    //add functionality to guess function here
+    //8)
+    var input = document.getElementById('user-guess');
+    if(answer.value == "" || attempt.value == "") {
+        setHiddenFields();
+    }
+
+    
+    //11)
+    if(!validateInput(input.value)){
+        return;
+    } else {
+        attempt.value++;
+    }
+    
+    
+    //14) 15) 16) 19)
+    if(getResults(input.value)) {
+        setMessage('You Win! :)');
+        showAnswer(true);
+        showReplay();
+    } else if(attempt.value >= 10) {
+        setMessage('You Lose! :(');
+        showAnswer(false);
+        showReplay();
+    } else {
+        setMessage('Incorrect, try again.');
+    }
 }
 
-function setHiddenFilds(){
-    answer = Math.floor(Math.random()*10000);
-    while(answer.value.length < 4){
+
+//12) 13)checking for correct guess 
+ function getResults(input){
+    var correct = 0;
+    var html = '<div class="row"><span class="col-md-6">' + input + '</span><div class="col-md-6">';
+    for(i = 0; i < input.length; i++)
+    {
+        if(input.charAt(i) == answer.value.charAt(i))
+        {
+            html += '<span class="glyphicon glyphicon-ok"></span>';
+            correct++;
+        } else if (answer.value.indexOf(input.charAt(i)) > -1) {
+            html += '<span class="glyphicon glyphicon-transfer"></span>';
+        } else {
+            html += '<span class="glyphicon glyphicon-remove"></span>';
+        }
+    }
+    html += '</div></div>';
+
+    document.getElementById('results').innerHTML += html;
+
+    if(correct == input.length) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+
+// 5) 6) 7)
+function setHiddenFields() {
+    answer.value = Math.floor(Math.random() * 10000).toString();
+    while(answer.value.length < 4) {
         answer.value += 0 + answer.value.toString();
     }
-} 
+    attempt.value = 0; 
+}
 
-//implement new functions here
+//9) 
+function setMessage(mess){
+    document.getElementById('message').innerHTML = mess;
+}
+
+
+//17)
+ function showAnswer(success){
+    let code = document.getElementById('code');
+    if(success) {
+        code.className += " success";
+    } else {
+        code.className += " failure";
+    }
+    code.innerHTML = answer.value;
+}
+
+
+//18)
+ function showReplay(){
+    document.getElementById('guessing-div').style.display = "none";
+    document.getElementById('replay-div').style.display = "block";
+}
+
+
+
+//10) 
+function validateInput(input) {
+    if(input.length != 4) {
+        setMessage('Guesses must be exactly 4 characters long.');
+        return false;
+    } 
+    return true;
+}
