db89e4ecd731066050229e83db9d434d233eb850
Taleb01 <taleb_n1@hotmail.com>
2017-11-21 11:06:35 +0100

    Added first function 'SetHiddenFilds'

--- src/assets/main.js
+++ src/assets/main.js
@@ -6,4 +6,11 @@ function guess() {
     //add functionality to guess function here
 }
 
+function setHiddenFilds(){
+    answer = Math.floor(Math.random()*10000);
+    while(answer.value.length < 4){
+        answer.value += 0 + answer.value.toString();
+    }
+} 
+
 //implement new functions here
